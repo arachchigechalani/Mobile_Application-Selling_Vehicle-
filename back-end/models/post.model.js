7dcc0c1a3f7efa0f60e9ad51f4861cbb99adebfc
arachchigechalani <arachchigechalani@gmail.com>
2022-10-05 21:48:16 +0530

    updated project

--- back-end/models/post.model.js
+++ back-end/models/post.model.js
@@ -3,7 +3,26 @@ const mongoose=require('mongoose')
 
 const postSchema=new mongoose.Schema({
 
-    
+    userId : {
+        type: ObjectId,
+        required: true
+    },
+    date : {
+        type: String,
+        required: true
+    },
+    time : {
+        type: String,
+        required :  true,
+    },
+    title: {
+        type :  String,
+        required : true
+    },
+    body: {
+        type : Object,
+        required : true
+    }
 
 
 })
